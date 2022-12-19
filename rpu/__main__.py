9795b60196518937e22782c441e9962465dbeb92
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2022-12-18 19:48:32 -0800

    Update __main__.py

--- rpu/__main__.py
+++ rpu/__main__.py
@@ -1,4 +1,7 @@
+import inspect
+import os
 import platform
+import shutil
 import sys
 import webbrowser
 
@@ -51,6 +54,87 @@ def cmd_system_info():
 
     nl = "\n"
     print(nl.join([f"{item}: {info[item]}" for item in info]))
+
+
+@client.command(
+    name="remove-dependency",
+    brief="removes the need to have rpu as a dependency",
+)
+def remove_dependency(folder: str, mode: str = "file"):
+    """removes rpu has a dependency in your python package
+
+    folder: the folder to be used
+    mode: the mode to be used.
+
+    Modes:
+        file - every part of rpu you use gets put into a single file
+        clone - rpu gets cloned into your package, though that allows users to do `your_package.rpu.something`
+    """
+
+    if not os.path.exists(folder):
+        return print(f"Folder {folder} not found")
+
+    if mode == "clone":
+        before = rpu.__file__.removesuffix("\\__init__.py").replace("\\", "/")
+        after = f"{folder}/rpu"
+        shutil.copytree(before, after)
+        print("Successfully cloned RPU")
+
+    elif mode == "file":
+        files = [file for file in os.listdir(f"{folder}") if str(file).endswith(".py")]
+        for file in files:
+            with open(f"{folder}/{file}", "r") as f:
+                contents = f.read()
+
+            import ast
+
+            tree = ast.parse(contents)
+            found = []
+
+            for item in tree.body:
+                if isinstance(item, ast.ImportFrom):
+                    if str(item.module).startswith("rpu"):
+                        found.extend([alias.name for alias in item.names])
+                elif isinstance(item, ast.Expr):
+                    if isinstance(item.value, ast.Call):
+                        if isinstance(item.value.func, ast.Attribute):
+                            if isinstance(item.value.func.value, ast.Name):
+                                if item.value.func.value.id:
+                                    found.append(item.value.func.attr)
+                            elif isinstance(item.value.func.value, ast.Attribute):
+                                if isinstance(item.value.func.value.value, ast.Name):
+                                    if item.value.func.value.value.id == "rpu":
+                                        found.append(item.value.func.attr)
+
+                    elif isinstance(item.value, ast.Attribute):
+                        if isinstance(item.value.value, ast.Name):
+                            if item.value.value.id == "rpu":
+                                found.append(item.value.attr)
+                        elif isinstance(item.value.value, ast.Attribute):
+                            if isinstance(item.value.value.value, ast.Name):
+                                if item.value.value.value.id == "rpu":
+                                    found.append(item.value.attr)
+
+            if not found:
+                return print(f"{file} does not use rpu")
+            print(f'Found: {", ".join(found)}')
+
+            final = []
+
+            for item in found:
+                try:
+                    source_lines, _ = inspect.getsourcelines(getattr(rpu, item))
+                    final.append("".join(source_lines))
+                except Exception as e:
+                    print(f"Could not find source code for {item}")
+                    raise e
+
+            with open(f"{folder}/rpu.py", "w") as f:
+                f.write("\n\n".join(final))
+            print("Done")
+
+    else:
+        return print("Invalid Mode. Valid Modes: file, clone")
 
 
 client.run()
