3e1349646f4268f112e8dae584f16a7c697d65c0
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2023-01-02 15:46:40 -0800

    added percentage functions

--- rpu/numbers.py
+++ rpu/numbers.py
@@ -81,3 +81,64 @@ def word_to_int(word: str, /) -> Union[int, str]:
         "eight": 8,
         "nine": 9,
     }.get(word, word)
+
+
+def get_percentage(num1: Union[int, float], num2: Union[int, float], /) -> float:
+    """Returns the percentage of the smaller number, when it comes to the bigger number
+
+    Parameters
+    ----------
+    number1: Union[`int`, `float`]
+        the first number
+    number2: Union[`int`, `float`]
+        the second number
+
+    Useage
+    ----------
+    ... get_percentage(5, 50)
+    >>> 10.0
+
+    ... get_percentage(25.123, 1000)
+    >>> 2.5123
+
+    Returns
+    ----------
+    float
+        the percentage
+    """
+
+    if num1 > num2:
+        big = num1
+        small = num2
+    else:
+        big = num2
+        small = num1
+
+    return 100 * float(small) / float(big)
+
+
+def get_percent_of(percentage: Union[int, float], num: Union[int, float]) -> float:
+    """Returns the percentage given, of the number given
+
+    Parameters
+    ----------
+    number1: Union[`int`, `float`]
+        the first number
+    number2: Union[`int`, `float`]
+        the second number
+
+    Useage
+    ----------
+    ... get_percent_of(10, 50)
+    >>> 5.0
+
+    ... get_percent_of(2.5123, 1000)
+    >>> 25.123
+
+    Returns
+    ----------
+    float
+        the percentage
+    """
+
+    return (percentage * num) / 100.0
